66b9aec0258aac2cd1a9b1b2c435473da639b9f1
Nick Theodoridis <122683142+nicktheodoridisiOS@users.noreply.github.com>
2023-02-25 20:43:46 +0200

    Import the project

---
+++ animated-splash-screen/animated-splash-screen/animated_splash_screenApp.swift
@@ -0,0 +1,17 @@
+//
+//  animated_splash_screenApp.swift
+//  animated-splash-screen
+//
+//  Created by Nick Theodoridis on 25/2/23.
+//
+
+import SwiftUI
+
+@main
+struct animated_splash_screenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ animated-splash-screen/animated-splash-screenUITests/animated_splash_screenUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  animated_splash_screenUITestsLaunchTests.swift
+//  animated-splash-screenUITests
+//
+//  Created by Nick Theodoridis on 25/2/23.
+//
+
+import XCTest
+
+final class animated_splash_screenUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
